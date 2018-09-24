fe64147b28011ead4a8d3f88f617ada603f3eaca
developer <boris-plus@mail.ru>
2018-09-24 19:56:23 +0300

    Удалил разные пути

--- kiosk/kiosk/urls.py
+++ kiosk/kiosk/urls.py
@@ -1,33 +1,17 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.http import (
-    HttpResponseRedirect,
+from django.views.generic import (
+    RedirectView
 )
-try:
-    from kiosk.store_app import views as store_app_views
-except:
-    from store_app import views as store_app_views
-
-
-def admin_default(request):
-    if request:
-        pass
-    return HttpResponseRedirect('/admin/store_app/product/')
-
-
-def default(request):
-    if request:
-        pass
-    return HttpResponseRedirect('/product_list/')
+from store_app import views as store_app_views
 
 
 urlpatterns = [
     url(r'^admin/logout/$', auth_views.logout, {'next_page': '/admin'}, name="logout"),
-    url(r'^admin$', admin_default),
+    url(r'^admin$', RedirectView.as_view(url='/admin/store_app/product/')),
     url(r'^admin/', admin.site.urls),
-    url(r'^product_list/$', store_app_views.ProductList.as_view()),
-    url(r'^product_detail/(?P<pk>\d+)/$', store_app_views.ProductDetail.as_view()),
-    url(r'^test/$', store_app_views.test),
-    url(r'', default),
+    url(r'^product_list/$', store_app_views.ProductList.as_view(), name="product_list"),
+    url(r'^product_detail/(?P<pk>\d+)/$', store_app_views.ProductDetail.as_view(), name="product_detail"),
+    url(r'', RedirectView.as_view(url='/product_list/')),
 ]
